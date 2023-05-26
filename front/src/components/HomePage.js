1aba2bc2400e9edc51dd6f7cb0526cd171198875
ArianaRexhepi <119941710+ArianaRexhepi@users.noreply.github.com>
2023-05-26 17:54:55 +0200

    first book added

--- front/src/components/HomePage.js
+++ front/src/components/HomePage.js
@@ -2,22 +2,37 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './homepage.css';
 import SearchBar from './SearchBar';
+import image1 from './images/image1.jpg';
 
 function HomePage() {
-    const handleSearch = (searchTerm) => {
-      console.log('Search term:', searchTerm);
-      // Perform search logic here (e.g., make an API call)
-    };
-  
-    return (
-      <div>
-        <br></br>
-        <h1><b><span>MJ </span> Library</b></h1>
-        <p>The world's largest online library!</p>
-        <SearchBar onSearch={handleSearch} />
+  const handleSearch = (searchTerm) => {
+    console.log('Search term:', searchTerm);
+    // Perform search logic here (e.g., make an API call)
+  };
+
+  return (
+    <div>
+      <br></br>
+      <h1>
+        <b>
+          <span>MJ </span> Library
+        </b>
+      </h1>
+      <p>The world's largest online library!</p>
+      <SearchBar onSearch={handleSearch} />
+
+      <div className="flex-container">
+        <div className="flex-item">
+          <div className="box">
+            <img
+              src={image1}
+              alt="Image 1"
+              style={{ height: '300px', width: '300px' }} />
+          </div>
+        </div>
       </div>
-    );
-  }
-  
-  export default HomePage;
-  
+    </div>
+  );
+}
+
+export default HomePage;
