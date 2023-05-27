c41d6a772b9c43b96dc8326d591dc543b64ea4f7
ArianaRexhepi <119941710+ArianaRexhepi@users.noreply.github.com>
2023-05-27 18:12:13 +0200

    search button

--- front/src/components/SearchBar.js
+++ front/src/components/SearchBar.js
@@ -22,7 +22,7 @@ const SearchBar = ({ onSearch }) => {
         placeholder="Search..."
         style={{ width: '750px', height: '40px' }}
       />
-      <button type="submit" style={{ backgroundColor: 'blue', color: 'white', border: '1px solid white'}}>Search</button>
+      <button type="submit" style={{ backgroundColor: 'blue', color: 'white', border: '2px solid white', width: '100px', height: '40px' }}>Search</button>
     </form>
   );
 };
