07e52f1c054e17bedbc90e0fd371471bc1c3305f
FestaRexhepi <festarexhepi783@gmail.com>
2023-06-03 23:42:02 +0200

    import image15

--- front/src/components/BookUserList.js
+++ front/src/components/BookUserList.js
@@ -10,6 +10,8 @@ import image11 from "./images/image11.jpeg";
 import image12 from "./images/image12.jpg";
 import image13 from "./images/image13.jpeg";
 import image14  from "./images/image14.jpg";
+import image15  from "./images/image15.jpg";
+
 
 
 const books = [
