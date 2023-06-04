3454a56a3a939450ed01c92efd00cd3f9463db73
ArianaRexhepi <119941710+ArianaRexhepi@users.noreply.github.com>
2023-06-04 18:40:42 +0200

    adding to the edit book

--- front/src/components/Books/EditBook.js
+++ front/src/components/Books/EditBook.js
@@ -1,35 +1,131 @@
 import React, { useState } from 'react';
 import axios from 'axios';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate, useParams } from 'react-router-dom';
+
+function EditBook() {
+    const [loading, setLoading] = useState(false);
+    const [book, setBook] = useState(null);
+    const navigate = useNavigate();
+    const { id } = useParams();
+
+    useEffect(() => {
+        axios
+          .get(`http://localhost:5267/api/books/${id}`)
+          .then((response) => {
+            setBook(response.data);
+            console.log(response.data);
+          });
+      }, []);
+
+      const handleSubmit = async (event) => {
+        event.preventDefault();
+        setLoading(true);
+    
+        try {
+          await axios
+            .put(`http://localhost:5267/api/books/${id}`, book)
+            .then(() => {
+              setLoading(false);
+              navigate("/books");
+            });
+        } catch (error) {
+          console.error(error);
+        }
+      };
 
 function EditBook() {
     return (
-        <div className="modal-dialog" style={{width: 600}}>
-            <div className="modal-content">
-                <form className="form">
-                    <div className="modal-header">
-                        <h4 className="modal-title">Edito Libër</h4>
-                        <Link to="/books"><button type="button" className="close" data-dismiss="modal" aria-hidden="true" onclick="window.location='/books';">&times;</button></Link>
-                    </div>
-                    <div className="modal-body">
-                        <div className="form-group">
-                            <label>Titulli:</label>
-                            <input type="text" className="form-control" />
-                        </div>
-                        <div className="form-group">
-                            <label>Autori:</label>
-                            <input type="text" className="form-control" />
-                        </div>
-                    </div>
-
-                    <div className="modal-footer">
-                        <Link to="/books"><input type="button" className="btn btn-dark" value="Anulo" /></Link>
-                        <input type="submit" value="Edito" className="btn btn-primary float-right" />
-                    </div>
-                </form>
+        <div className="modal-dialog" style={{ width: 600, marginTop:'50px' }}>
+        <div className="modal-content">
+          <form className="form">
+            <div className="modal-header">
+              <h4 className="modal-title">Edit Book</h4>
+              <Link to="/bestsellers">
+                <button
+                  type="button"
+                  className="close"
+                  data-dismiss="modal"
+                  aria-hidden="true"
+                >
+                  &times;
+                </button>
+              </Link>
             </div>
+            <div className="modal-body">
+              <div className="form-group">
+                <label>Title:</label>
+                <input
+                  type="text"
+                  className="form-control"
+                  value={book.title}
+                  onChange={(e) =>
+                    setBook({ ...book, title: e.target.value })
+                  }
+                />
+              </div>
+              <div className="form-group">
+                <label>Autori:</label>
+                <input
+                  type="text"
+                  className="form-control"
+                  value={bestseller.author}
+                  onChange={(e) =>
+                    setBestseller({ ...bestseller, author: e.target.value })
+                  }
+                />
+              </div>
+              <div className="form-group">
+                <label>Viti:</label>
+                <input
+                  type="datetime-local"
+                  className="form-control"
+                  value={bestseller.year}
+                  onChange={(e) =>
+                    setBestseller({ ...bestseller, year: e.target.value })
+                  }
+                />
+              </div>
+              <div className="form-group">
+                <label>Rating:</label>
+                <input
+                  type="text"
+                  className="form-control"
+                  value={bestseller.rating}
+                  onChange={(e) =>
+                    setBestseller({ ...bestseller, rating: e.target.value })
+                  }
+                />
+              </div>
+              <div className="form-group">
+                <label>Image:</label>
+                <input
+                  type="text"
+                  className="form-control"
+                  value={bestseller.image}
+                  onChange={(e) =>
+                    setBestseller({ ...bestseller, image: e.target.value })
+                  }
+                />
+              </div>
+            </div>
+  
+            <div className="modal-footer">
+              <Link to="/bestsellers">
+                <input type="button" className="btn btn-dark" value="Dismiss" />
+              </Link>
+              <input
+                onClick={handleSubmit}
+                type="submit"
+                disabled={loading}
+                value="Edit"
+                className="btn btn-primary float-right"
+              />
+            </div>
+          </form>
         </div>
-    );
+      </div>
+        );
+    }
 }
 
 export default EditBook;
