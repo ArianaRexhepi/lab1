e9ff91359e5a144e6c265b9ca9b23c3330c72937
ArianaRexhepi <119941710+ArianaRexhepi@users.noreply.github.com>
2023-06-05 16:28:48 +0200

    changes

--- front/src/components/HomePage.js
+++ front/src/components/HomePage.js
@@ -1,13 +1,13 @@
-import React, { useState } from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './homepage.css';
-import SearchBar from './SearchBar';
-import image1 from './images/image1.jpg';
-import image2 from './images/image2.jpeg';
-import image3 from './images/image3.jpg';
-import image4 from './images/image4.jpeg';
-import image5 from './images/image5.jpeg';
-import image6 from './images/image6.jpg';
+import React, { useState } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./homepage.css";
+import SearchBar from "./SearchBar";
+import image1 from "./images/image1.jpg";
+import image2 from "./images/image2.jpeg";
+import image3 from "./images/image3.jpg";
+import image4 from "./images/image4.jpeg";
+import image5 from "./images/image5.jpeg";
+import image6 from "./images/image6.jpg";
 
 function HomePage() {
   const [searchResults, setSearchResults] = useState([]);
@@ -30,28 +30,47 @@ function HomePage() {
       <div className="flex-container">
         <div className="flex-item">
           <div className="box">
-            <img src={image1} alt="Image 1" style={{ height: '250px', width: '200px' }} />
-            <img src={image2} alt="Image 1" style={{ height: '250px', width: '200px' }} />
-            <img src={image3} alt="Image 3" style={{ height: '250px', width: '200px' }} />
+            <img
+              src={image1}
+              alt="Image 1"
+              style={{ height: "250px", width: "200px" }}
+            />
+            <img
+              src={image2}
+              alt="Image 1"
+              style={{ height: "250px", width: "200px" }}
+            />
+            <img
+              src={image3}
+              alt="Image 3"
+              style={{ height: "250px", width: "200px" }}
+            />
           </div>
           <div className="box">
-            <img src={image4} alt="Image 4" style={{ height: '250px', width: '200px' }} />
-            <img src={image5} alt="Image 5" style={{ height: '250px', width: '200px' }} />
-            <img src={image6} alt="Image 6" style={{ height: '250px', width: '200px' }} />
+            <img
+              src={image4}
+              alt="Image 4"
+              style={{ height: "250px", width: "200px" }}
+            />
+            <img
+              src={image5}
+              alt="Image 5"
+              style={{ height: "250px", width: "200px" }}
+            />
+            <img
+              src={image6}
+              alt="Image 6"
+              style={{ height: "250px", width: "200px" }}
+            />
           </div>
         </div>
       </div>
       <br />
-      <div className='about-us'>
-      <h2>About Us</h2>
-      <p>Welcome to MJ Library, the world's largest online library!</p>
-      <p>Learn more about our mission and values.</p>
-      <br />
-      </div>
-      <hr></hr>
       <footer>
         <p>
-          <i>@2023 by <span>MJ Library.</span> All Rights Reserved.</i>
+          <i>
+            @2023 by <span>MJ Library.</span> All Rights Reserved.
+          </i>
         </p>
       </footer>
     </div>
