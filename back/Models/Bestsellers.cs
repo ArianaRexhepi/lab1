c11bc107aa6b83cac1170e676834851ab2c2786b
ArianaRexhepi <119941710+ArianaRexhepi@users.noreply.github.com>
2023-06-05 18:07:18 +0200

    added attribute

--- back/Models/Bestsellers.cs
+++ back/Models/Bestsellers.cs
@@ -7,6 +7,7 @@ namespace back.Models
         public int Id { get; set; }
         public string Title { get; set; }
         public string Author { get; set; }
+        public string Description { get; set; }
         public string Rating { get; set; }
         public DateTime Year { get; set; }
         public string Image { get; set; }
