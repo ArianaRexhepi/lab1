6222116f95de7856b4ac8f115a941aa6c5be0fb8
ArianaRexhepi <119941710+ArianaRexhepi@users.noreply.github.com>
2023-06-12 14:03:18 +0200

    changes

--- front/src/components/Recbooks.js
+++ front/src/components/Recbooks.js
@@ -2,9 +2,15 @@ import React from "react";
 
 const Recbooks = () => {
   return (
-    <div>
-      <h3>Recommended books</h3>
-      <p>Here are some recommended books since you like this genre.</p>
+    <div className="rec-books-container">
+      <div className="rec-books-header">
+        <h3>Recommended Books</h3>
+      </div>
+      <div className="rec-books-content">
+        <p>
+          We believe that books have the power to inspire, educate, and entertain. Whether you're a seasoned reader or just starting your literary journey, this carefully curated selection of books is designed to provide you with a diverse range of captivating reads.
+        </p>
+      </div>
     </div>
   );
 }
